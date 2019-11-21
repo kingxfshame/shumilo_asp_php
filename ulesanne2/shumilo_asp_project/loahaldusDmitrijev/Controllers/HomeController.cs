da85d5fa60790b2370588fb77225116f35dabbaf
Opilane TTHK <opilane@tthk.ee>
2019-11-21 11:26:06 +0200

    filer_ready

--- ulesanne2/shumilo_asp_project/loahaldusDmitrijev/Controllers/HomeController.cs
+++ ulesanne2/shumilo_asp_project/loahaldusDmitrijev/Controllers/HomeController.cs
@@ -9,12 +9,82 @@ namespace shumilo_asp_project.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Index(string filterbynimi = "ASC",string[] checkbox_check = null)
         {
-            ViewBag.Title = "Home Page";
+
             DataBaseContext db = new DataBaseContext();
+
+            ViewBag.names = db.Nimed.OrderByDescending(x => x.eestoni_nimi);
+
+            ViewBag.Title = "Home Page";
+
             ViewBag.user = db.Users;
-            ViewBag.names = db.Nimed;
+
+            List<string> letters = new List<string>();
+            for (char letter = 'A'; letter <= 'Z'; letter++)
+            {
+                letters.Add(Convert.ToString(letter));
+            }
+            
+            ViewBag.Letters = letters;
+            ViewBag.Letters_Count = letters.Count;
+            ViewBag.Checkbox = new string[letters.Count];
+            if(checkbox_check == null)
+            {
+                checkbox_check = ViewBag.Letters.ToArray();
+            }
+            for (int i = 0; i < letters.Count; i++)
+            {
+                if (checkbox_check.Contains(letters[i]))
+                    ViewBag.Checkbox[i] = "checked";
+
+            }
+            List<Nimi> selected = new List<Nimi>();
+
+            for (int i = 0; i < checkbox_check.Length; i++)
+            {
+                if (filterbynimi == "ASC")
+                    selected.AddRange ( db.Nimed.ToList().Where(x => x.eestoni_nimi[0].ToString() == checkbox_check[i]).OrderBy(x => x.eestoni_nimi));
+                else if (filterbynimi == "DESC")
+                    selected.AddRange(db.Nimed.ToList().Where(x => x.eestoni_nimi[0].ToString() == checkbox_check[i]).OrderByDescending(x => x.eestoni_nimi));
+
+
+            }
+            ViewBag.names = selected;
+
+
+            //if (filterbynimi == "ASC")
+            //{
+            //    ViewBag.names = db.Nimed.ToList().Where(x => x.eestoni_nimi[0].ToString() == checkbox_a  ||  x.eestoni_nimi[0].ToString() == checkbox_b  || x.eestoni_nimi[0].ToString() == checkbox_c).OrderBy(x => x.eestoni_nimi);
+            //    ViewBag.asc = "selected";
+            //    ViewBag.desc = "";
+
+            //    if (checkbox_a != "A")
+            //        ViewBag.a = "";
+
+            //    if (checkbox_b != "B")
+            //        ViewBag.b = "";
+            //    if (checkbox_c != "C")
+            //        ViewBag.c = "";
+            //}
+
+            //else if (filterbynimi == "DESC")
+            //{
+            //    ViewBag.names = db.Nimed.ToList().Where(x => x.eestoni_nimi[0].ToString() == checkbox_a || x.eestoni_nimi[0].ToString() == checkbox_b || x.eestoni_nimi[0].ToString() == checkbox_c).OrderByDescending(x => x.eestoni_nimi);
+            //    ViewBag.asc = "";
+            //    ViewBag.desc = "selected";
+
+            //    if (checkbox_a != "A")
+            //        ViewBag.a = "";
+
+            //    if (checkbox_b != "B")
+            //        ViewBag.b = "";
+            //    if (checkbox_c != "C")
+            //        ViewBag.c = "";
+            //}
+
+
+
             //ViewBag.user = db.Users.Where(x => x.login == "admin").FirstOrDefault();
             return View();
         }
